8d529e5950392e358009b99d8d22f1654708c53c
kaligar <kaligar@debian-terraform.debian-tf-23040011>
2025-07-10 14:11:12 -0700

    GRITO DE SOCORRO2

--- main.tf
+++ main.tf
@@ -1,5 +1,59 @@
-resource "aws_ssm_parameter" "foo"{
-	name = "foo"
-	type = "String"
-	value = "bar"
+locals {
+  private_key_path = "/home/kaligar/Downloads/terraform.pem" # Mejor ruta absoluta relativa al módulo
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "cloudfare_email" {}
+variable "cloudfare_token" {}
+
+variable "domain_name" {
+	default = "230400110.lindavdesign.com"
+}
+variable "email_admin" {
+	default = "23040011@utculiacan.edu.mx"
+}
+resource "aws_instance" "example" {
+  ami                    = "ami-02da2f5b47450f5a8"  # Debian 12 en us-east-2
+  instance_type          = "t2.micro"
+  key_name               = "terraform"
+  vpc_security_group_ids = ["sg-0ad10b5c714b930c3"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Esperando SSH...'",
+      "sudo apt update -y"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "admin" # Asegúrate de que sea el usuario correcto
+      private_key = file(local.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
+	provisioner "local-exec" {
+		command = "ansible-playbook -i \"${self.public_ip},\" --private-key \"${local.private_key_path}\" --extra-vars \"odoo_domain=${var.domain_name} odoo_email=${var.email_admin} public_ip_record=${self.public_ip} cloudfare_email=${var.cloudfare_email} cloudfare_token=${var.cloudfare_token}\" /home/kaligar/ansible/aws/aws-odoo.yml"
+	}
+}
+
+output "example_ip" {
+  value = aws_instance.example.public_ip
+}
+
