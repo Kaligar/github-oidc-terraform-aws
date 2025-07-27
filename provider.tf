dd75b8f4c0a200bd1fa5486f95ffb7a8d6f357c9
kaligar <federico.jose.05@hotmail.com>
2025-07-26 21:29:04 -0700

    asd2asd223324asfff2f

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "github-oidc-terraform-tfstate"
     key            = "github-oidc-terraform.tfstate"
-    region         = "ca-central-1"
+    region         = "us-east-2"
     encrypt        = true
   }
 }
